cdefd932e06ed88068979dd0b1d2a2f069b52e30
jesseb488 <148990836+jesseb488@users.noreply.github.com>
2025-04-15 13:20:42 -0400

    Updated hashmap.h with the collision resolution.

--- src/hashmap.h
+++ src/hashmap.h
@@ -1,7 +1,8 @@
 //
 // Created by diego on 4/14/2025.
 //
-
+// Modified by Jesse on 4/15/2025
+//
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
@@ -11,51 +12,84 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
+using namespace std;
 
-constexpr float MAX_LOAD = 0.80; // this can be modified if needed
+//Can modify max load
+constexpr float MAX_LOAD = 0.80;
 constexpr int SIZE_OF_HASH_MAP = 100;
+
+unsigned long long int HashFunction_one(const string& word);
 
 
 class HM_separateChaining {
 public:
-	int entries_counter = 0;
-	std::vector<std::pair<std::string, int>> map[SIZE_OF_HASH_MAP] = {};
-	//consider making this a pointer so that we can later "resize" the map by creating a new array and update the pointer
-	// current problem: cannot make array of 100000 elements.
+    int entries_counter = 0;
+    vector<pair<string, int>> a[SIZE_OF_HASH_MAP] = {};
 
-	void insert(const unsigned long long int hash_code_, const std::string& word_, int score_) {
-		a[hash_code_].push_back(std::pair<std::string, int>(word_, score_));
-		entries_counter++;
-	}
-	void search(const std::string& word_) {
-        int hash_code = HashFunction(word_);
-        for(int i = 0; i < this->map[hash_code].size(); i++) {
-          if(this->map[hash_code][i].first == word_) {
-            std::cout << "The score of the word" << this->map[hash_code][i].first <<"is: " << this->map[hash_code][i].second << std::endl;
-          }
-          if( i == this->map[hash_code].size() - 1 && this->map[hash_code][i].first != word_) {
-            std::cout << "The word: " << this->map[hash_code][i].first << " is not present in the map" << std::endl;
-          }
+    void insert(const unsigned long long int hash_code, const string& word, int score) {
+        a[hash_code].push_back(make_pair(word, score));
+        entries_counter++;
+    }
+
+    void search(const string& word) {
+        unsigned long long int hash_code = HashFunction_one(word);
+        bool found = false;
+        for (size_t i = 0; i < a[hash_code].size(); i++) {
+            if (a[hash_code][i].first == word) {
+                cout << "The score of the word " << a[hash_code][i].first
+                          << " is: " << a[hash_code][i].second << endl;
+                found = true;
+                break;
+            }
         }
-	}
+        if (!found) {
+            cout << "The word: " << word << " is not present in the map" << endl;
+        }
+    }
 };
 
 
 class HM_linearProbing {
 public:
-	int entries_counter = 0;
+    int entries_counter = 0;
+    vector<pair<string, int>> map[SIZE_OF_HASH_MAP] = {};
 
-	std::vector<std::pair<std::string, int>> map[SIZE_OF_HASH_MAP] = {};
+    void insert(const unsigned long long int hash_code, const string& word, int score) {
+        int idx = hash_code;
+        int start = idx;
+        while (!map[idx].empty()) {
+            if (map[idx][0].first == word) {
+                map[idx][0].second = score;
+                return;
+            }
+            idx = (idx + 1) % SIZE_OF_HASH_MAP;
+            if (idx == start) {
+                cerr << "Hash table is full." << endl;
+                return;
+            }
+        }
+        map[idx].push_back(make_pair(word, score));
+        entries_counter++;
+    }
 
-	void insert(const unsigned long long int hash_code_, const std::string& word_, int score_);
-	void search(const std::string& word_);
+    void search(const string& word) {
+        unsigned long long int hash_code = HashFunction_one(word);
+        int idx = hash_code;
+        int start = idx;
+        while (!map[idx].empty()) {
+            if (map[idx][0].first == word) {
+                cout << "The score of the word " << word
+                          << " is: " << map[idx][0].second << endl;
+                return;
+            }
+            idx = (idx + 1) % SIZE_OF_HASH_MAP;
+            if (idx == start) break;
+        }
+        cout << "The word: " << word << " is not present in the map" << endl;
+    }
 };
 
-unsigned long long int HashFunction_one(const std::string& word);
+void ProcessTweet_one(const string& tweet_, HM_separateChaining& a_map, const int score);
+void ParseDataFile(const string& fileName, HM_separateChaining& a_map);
 
-void ProcessTweet_one(const std::string& tweet_, HM_separateChaining& a_map, const int score);
-
-void ParseDataFile(const std::string& fileName, HM_separateChaining& a_map);
-
-
-#endif //HASHMAP_H
+#endif // HASHMAP_H
